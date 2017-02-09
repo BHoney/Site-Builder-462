cf7c1a31eb55b45816a20820d2e0183a2127464d
BloodyHoney <jstc.14@gmail.com>
2017-02-09 17:38:00 -0500

    Initial Commit

---
+++ Assets/PixelScanner.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PixelScanner : MonoBehaviour {
+
+	public Texture2D map;
+	private int map_height, map_width;
+	private Vector2 pos;
+	// Use this for initialization
+	void Start () {
+		GetMapData();
+	}
+	
+	 int[,] GetMapData()
+	{
+		int [,] mapData = new int[map.width,map.height];
+		int walker = 0;
+		Color[] mapPixelData = map.GetPixels();
+		//Debug.Log(mapPixelData[0]);
+		for(int y = 0; y > map.height; y++){
+			for(int x = 0; x > map.width*y; x++ ){
+				if(mapPixelData[walker] == Color.white){
+					mapData[x,y] = 0;
+				}
+				else if(mapPixelData[walker] == Color.black){
+					mapData[x,y] = 1;
+				}
+				walker++;
+			}
+		}
+		Debug.Log("Map Data Retrieved.");
+		return mapData;	
+		 }
+	}
---
+++ Assets/Point.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class Point : MonoBehaviour {
+	
+	public Point[] neighbors;
+	public bool selected;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		DrawWall();
+	}
+
+	/// <summary>
+	/// Automatically draws walls between all points. Currently only for debugging.
+	/// </summary>
+	void DrawWall(){
+		foreach (Point p in neighbors)
+		{
+			if(p!=null)
+			Debug.DrawLine(this.transform.position, p.transform.position, Color.green);
+		}
+	}
+	 /// <summary>
+	/// Called when the mouse enters the GUIElement or Collider.
+	/// </summary>
+	void OnMouseEnter()
+	{
+		this.GetComponentInParent<Renderer>().material.color = Color.blue;
+	}
+
+	/// <summary>
+	/// Called when the mouse is not any longer over the GUIElement or Collider.
+	/// </summary>
+	void OnMouseExit()
+	{
+		this.GetComponentInParent<Renderer>().material.color = Color.white;
+	}
+
+	/// <summary>
+	/// OnMouseDown is called when the user has pressed the mouse button while
+	/// over the GUIElement or Collider.
+	/// </summary>
+	void OnMouseDown()
+	{
+		selected = !selected;
+		Debug.Log(selected);
+	}
+}
+
+
+	

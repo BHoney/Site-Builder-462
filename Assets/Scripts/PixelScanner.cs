fa8570a3d7e28fe041a472a3ad484491b8df1aa0
BloodyHoney <jstc.14@gmail.com>
2017-02-12 19:39:05 -0500

    Added drawMap() for debugging purposes.

--- Assets/Scripts/PixelScanner.cs
+++ Assets/Scripts/PixelScanner.cs
@@ -19,23 +19,13 @@ public class PixelScanner : MonoBehaviour
     /// </summary>
     void OnDrawGizmos()
     {
-        if(binaryImage != null){
-            for(int y = 0; y < map.height; y++){
-                for(int x = 0; x < map.width; x++){
-                    if(binaryImage[x,y] == 1){
-                        Gizmos.color = Color.black;
-                    Vector3 pos = new Vector3(-map.width/2 + x, -map.height/2 +y, 0);
-                    Gizmos.DrawCube(pos, Vector3.one);
-                    }
-                }
-            }
-        }
+        drawMap();
     }
 
     /// <summary>
     /// Using the texture's pixel data, creates a new binary map using black and white values.
     /// </summary>
-    /// <returns>MapData</returns>
+    /// <returns>mapData</returns>
     public int[,] GetMapData()
     {
         int[,] mapData = new int[map.width, map.height]; //Create a new array to store all the data
@@ -61,5 +51,22 @@ public class PixelScanner : MonoBehaviour
        // Debug.Log(string.Format("Map Data: {0}, Color {1}", mapData[1, 1], mapPixelData[10]));
 	   
         return mapData;
+    }
+
+    /// <summary>
+    /// Draws the data extracted for the map for visualization. For debugging purposes.
+    /// </summary>
+    private void drawMap(){
+         if(binaryImage != null){
+            for(int y = 0; y < map.height; y++){
+                for(int x = 0; x < map.width; x++){
+                    if(binaryImage[x,y] == 1){
+                        Gizmos.color = Color.black;
+                    Vector3 pos = new Vector3(-map.width/2 + x, -map.height/2 +y, 0);
+                    Gizmos.DrawCube(pos, Vector3.one);
+                    }
+                }
+            }
+        }
     }
 }

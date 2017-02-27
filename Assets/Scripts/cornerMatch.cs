8a7238573dfb9ea67f2ed42f340eb3199140cea6
Angel <angelrodriguez@Angels-MacBook-Pro.local>
2017-02-26 22:17:08 -0500

    corner maching script added

--- Assets/Scripts/cornerMatch.cs
+++ Assets/Scripts/cornerMatch.cs
@@ -42,44 +42,109 @@ public class cornerMatch : MonoBehaviour {
 		int[, ]topLeft =
 			{{1, 1, 1}, 
 			{1, 0,  0}, 
-			{1, 0,  0}};
+			{1, 0,  1}};
 
 		int[,] topRight= 
 		{{1, 1, 1}, 
 		{  0,  0, 1}, 
-		{  0,  0, 1}};
+		{  1,  0, 1}};
 
 
 		int[,] botLeft = 
-		{{1, 0, 0,}, 
+		{{1, 0, 1,}, 
 		{1, 0, 0,}, 
 		{1, 1, 1}};
 
 
 		int[,] botRight = 
-		{{ 0, 0, 1}, 
+		{{ 1, 0, 1}, 
 		{  0, 0, 1}, 
 		{1, 1,  1}};
 
-		//go through every array in the image array
-		for(int i = 1; i < image.Length -1; ++i) {
-			// j imcrements through every individual array
+		//every column in the array
+		for(int i = 1; i < image.GetLength(0)-1; ++i) {
+			// every row in the array
 			for (int j = 1; j < image.GetLength(0)-1; ++j) {
-				int matchcount = 0;
-				//k and m are used to check the amount of ones touching
+
+				// counter for each corner
+				int topLeftMatchCount = 0;
+				int topRightMatchCount = 0;
+				int botLeftMatchCount = 0;
+				int botRightMatchCount = 0;
+				//k and m are used to check compare to the preset arrays
 				for(int k = 0;k < 3;++k){
 					for(int m = 0;m < 3;++m){
+
+						//if it is a match with pattern increment pattern count
 						if(image[i+k-1,j+m-1] == topLeft[k,m]){
-//
-							++matchcount;
+							//		                            	 System.out.println("MATCHED ");
+							//		                            	 int x = i+k-1;
+							//		                            	 int y = j+m-1;                    	 
+							//		                            	 System.out.println("image ["+ x + "] [" + y+"]: " + image[i+k-1][j+m-1] + 
+							//		                            			 "\ncorner ["+k+"] ["+m+"]: " + topLeft[k][m]);
+
+							++topLeftMatchCount;            
 						}
+
+						if(image[i+k-1,j+m-1] == topRight[k,m]){
+
+							++topRightMatchCount;
+						}
+
+						if(image[i+k-1,j+m-1] == botLeft[k,m]){
+
+							++botLeftMatchCount;
+						}
+
+						if(image[i+k-1,j+m-1] == botRight[k,m]){
+
+							++botRightMatchCount;
+						}
+
 					}
 				}
-				if(matchcount == 9){
-					Debug.Log("Found corner1 pattern at " + i + ", " + j);
+
+				// if full match with pattern change vale accordingly 
+				if(topLeftMatchCount == 9){
+					Debug.Log("Found topLeft pattern at " + i + ", " + j);
+					//		                  9 = top 7 = left
+					image[i,j] = 97;
 				}
+
+				if(topRightMatchCount == 9){
+					Debug.Log("Found topRight pattern at " + i + ", " + j);
+					//		            	  top = 9 right = 8
+					image[i,j] = 98;
+				}
+
+				if(botLeftMatchCount == 9 ){
+					Debug.Log("Found botLeft pattern at " + i + ", " + j);
+					//		            	  bot = 2 left = 7
+					image[i,j] = 27;  
+				}
+
+				if(botRightMatchCount == 9 ){
+					Debug.Log("Found botRight pattern at " + i + ", " + j);
+					//		            	  bot = 2 right = 8
+					image[i,j] = 28;  
+				}
+
 			}
 		}
 	} 
+
+	public void Start(){
+		cornerMatcher (image);
+//		print (image);
+
+//		for (int r = 0; r < image.GetLength(0); r++) {
+//			for (int p = 0; p < image.GetLength(1); p++)  {
+//				print (r);
+//				print( image[r]);
+//			}
+//			print("\n");
+//		}
+////		Debug.Log();
+	}
 
 }

3749d0e3390d0d084ce5428b2b46407049cc0e6c
Angel <rodriguezaa@g.cofc.edu>
2017-04-13 17:42:44 -0400

    single cube attempt

--- Assets/Scripts/WallPoints.cs
+++ Assets/Scripts/WallPoints.cs
@@ -14,14 +14,15 @@ public class WallPoints : MonoBehaviour {
 		List<Vector3> wallPoints = new List<Vector3>();
 		//i: number of rows
 		for (int i = 0; i <= image.GetLength(0)-1; ++i) {
+			print ("rows " + image.GetLength (0) );
 			// j: number of cols
 			for (int j = 0; j <= image.GetLength(1)-1; ++j) {
-
+				print ("col " + image.GetLength (1) );
 				// if the it is a black pixel(0) add it to the Vector3 array
 				if(image[i,j] == 0){
 
 
-					wallPoints.Add(new Vector3 (i,j,40));
+					wallPoints.Add(new Vector3 (j,i,20));
 
 				}  
 				// else {
@@ -94,23 +95,84 @@ public class WallPoints : MonoBehaviour {
 //	}
 
 
+//	preps the list for yPointLength: only contains y Neighbors 
+	public List<Vector3> yPrep(List<Vector3> yList){
+
+//		for(int i =0; i < ){
+//			f
+//		}
+//	}
+
+		List<Vector3> y = new List<Vector3> ();
+		for(int i =0; i < yList.Count-1;i++){
+			Vector3 v1 = yList.ElementAt (i);
+////			if(i != yList.Count -2 ){
+//				
+//				if (i != yList.Count -1) {//
+//					Vector3 v2 = yList.ElementAt (i +1);
+//
+//					if (v1.x != v2.x) {
+//						yList.Remove (v2);
+//					}
+//
+//				}
+////			}
+			/// 
+		
+		
+			if (i < yList.Count - 2) {
+
+				Vector3 v3 = yList.ElementAt (i + 2); 
+				if (i < yList.Count - 1) {
+					Vector3 v2 = yList.ElementAt (i + 1);
+						if(v1.x != v2.x && v2.x != v3.x ){
+							yList.Remove (v2);
+						}
+
+					}
+					
+				}
+
+			}
+
+			
+//			else{
+//				Vector3 v2 = yList.ElementAt (i );
+//				if (v1.x != v2.x) {
+//					yList.Remove (v2);
+//
+//				}
+//
+//			}
+//		
+//		}
+		return yList;
+	}
 
 
+//	returns a list containing the continous line segment of the point from the list
+//	TODO: error point is not in the list 
 	public List<Vector3> xPointLength(List<Vector3> points, Vector3 point){
 		List<Vector3> listLen = new List<Vector3>();
 		int pos = points.IndexOf (point);
-		listLen.Add (point);
+		print ("pos  " + pos);
+		int i =0;
+//		for (int i = 0; i < points.Count-1; i++) {
+		while(i+pos < points.Count -1){
+			while(i+pos < points.Count){
+				print ("i: " + i);
+				Vector3 v1 = points.ElementAt (pos+i);
+				Vector3 v2 = points.ElementAt (pos+i +1);
 
-		for (int i = 0; i < points.Count; i++) {
-			
-			Vector3 v1 = points.ElementAt (pos+i);
-//			Vector3 v2 = points.ElementAt (pos+i);
-
-			if (point.x == v1.x) {
-				listLen.Add (v1);
-			} 
-			else {
-				return listLen;
+				if (point.y == v1.y && v1.x-v2.x > -2 ) {
+					
+					listLen.Add (v2);
+				} 
+				else {
+					listLen.Add (point);
+					return listLen;
+				}
+				i++;
 			}
 		}
 
@@ -118,28 +180,67 @@ public class WallPoints : MonoBehaviour {
 	}
 
 
-	public List<Vector3> yPointLength(List<Vector3> points, Vector3 point){
-		List<Vector3> listLen = new List<Vector3>();
-		int pos = points.IndexOf (point);
-		listLen.Add (point);
+//	public List<Vector3> yPointLength(List<Vector3> points, Vector3 point){
+//		List<Vector3> listLen = new List<Vector3>();
+//		int pos = points.IndexOf (point);
+//		print ("pos  " + pos);
+////		listLen.Add (point);
+//
+//		for (int i = pos; i < points.Count; i++) {
+//
+//			if (i == points.Count - 1) {
+//				return listLen;
+//				
+//			} else {
+//					Vector3 v1 = points.ElementAt (i);
+//					//			Vector3 v2 = points.ElementAt (pos+i);
+//
+//					if (point.y == v1.y) {
+//						listLen.Add (v1);
+//					} else {
+//						return listLen;
+//					}
+//				}
+//			}
+//
+//			
+//		return listLen;
+//	}
 
-		for (int i = 0; i < points.Count; i++) {
 
-			Vector3 v1 = points.ElementAt (pos+i);
-			//			Vector3 v2 = points.ElementAt (pos+i);
+////	returns an image with only point with no neighbors
+//	public int[,] noNeighIMG(int [,] image){
+//
+//
+//	}
 
-			if(point.y == v1.y){
-				listLen.Add(v1);
-			}
-			else {
-				return listLen;
+	//	returns an image containing only the y points(more than 0 neighbors? more than 1 neighbor)
+	public int[,] yIMG(int [,] image){
+		//every column in the array
+
+		for (int i = 0; i <= image.GetLength (0) - 2; ++i) {
+			// every row in the array
+
+			for (int j = 0; j <= image.GetLength (1) - 1; ++j) {
+//				if(i == image.GetLength (0) - 1){
+//				print("yyyy");
+				if (image [i, j] == 0 && image[i+1,j] == 0) {
+//					print("( " + i +", " + j + " )");
+					image [i, j] = 0;
+				} else {
+					image [i, j] = 1;
+				}
+//				}
+
 			}
 		}
-			
-		return listLen;
+		return image;
 	}
 
 
+//	//	returns an image containing only the x points(more than 0 neighbors? more than 1 neighbor)
+//	public int[,] xIMG(int [,] image){
+//	}
 
 
 
@@ -284,16 +385,29 @@ public class WallPoints : MonoBehaviour {
 
 		} ;
 
-		int[,] image6 = {	
-			{ 0, 0 ,0, 0, 0, 0},
-			{ 0, 1 ,1, 1, 1, 1},
-			{ 0, 1 ,1, 1, 1, 1},
-			{ 0, 1 ,1, 1, 1, 1},
+		int[,] imagey = {	
+			{ 0, 0 ,1, 0, 1, 1},
+			{ 0, 0 ,1, 0, 1, 1},
+			{ 0, 0 ,1, 0, 1, 1},
+			{ 0, 0 ,1, 0, 1, 1},
 
 		} ;
 
-			List<Vector3> points = getWallPoints(image6);
-//			printList(points); //for testing
+		int[,] imagex = {	
+			{ 0, 0 ,0, 0, 0, 0},
+			{ 0, 0 ,0, 0, 0, 0},
+			{ 1, 1 ,1, 1, 1, 1},
+			{ 0, 0 ,0, 0, 0, 0},
+
+		} ;
+
+
+//		int[,] m = yIMG(imagey);
+//		List<Vector3> y = getWallPoints (m);
+//
+//		printList (y);
+		List<Vector3> points = getWallPoints(image3);
+//			   printList(points); //for testing
 
 //		draw a Cube for each point
 				foreach(Vector3 point in points)
@@ -302,11 +416,14 @@ public class WallPoints : MonoBehaviour {
 			Instantiate(bube, point, Quaternion.identity);
 				}
 
-		Vector3 p = new Vector3 (0, 3, 40);
+//		Vector3 p = new Vector3 (0, 0, 20);
 
-		List<Vector3> pp = xPointLength (points, p);
+//		List<Vector3> pp = xPointLength (points, p);
+////
+//		printList (pp);
 
-		printList (pp);
+//		List<Vector3> y = yPrep (points);
+
 
 //		List<Vector3> py = yPointLength (points, p);
 //
@@ -316,9 +433,9 @@ public class WallPoints : MonoBehaviour {
 
 //		get rid of the original cube drawn
 //		bube.SetActive(false); //only hides the cube
-//			Destroy(bube);
+			Destroy(bube);
 
-		bube.transform.Rotate (0,0,-200);
+//		bube.transform.Rotate (0,0,-200);
 	}
 
 

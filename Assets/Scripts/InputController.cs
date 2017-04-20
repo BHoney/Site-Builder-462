39a40d62de7f97ccebea291c9bbfcb2d9a90f34c
bhoney <jstc.14@gmail.com>
2017-04-19 23:52:05 -0400

    UI is now hidden when object is not selected

--- Assets/Scripts/InputController.cs
+++ Assets/Scripts/InputController.cs
@@ -47,6 +47,9 @@ public class InputController : MonoBehaviour
                 print(selected.name);
                 PopulateUI(selected);
             }
+            else{
+                can.gameObject.SetActive(false);
+            }
         }
 
 
